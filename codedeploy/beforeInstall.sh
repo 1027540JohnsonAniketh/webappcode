43e5445d813a438c497e18ad00c3fdeb307b42f0
1027540JohnsonAniketh <njaniketh@gmail.com>
2021-11-04 00:27:02 -0700

    dev commit V3

--- codedeploy/beforeInstall.sh
+++ codedeploy/beforeInstall.sh
@@ -3,6 +3,3 @@ hangingJavaProcessToStop=`jps | grep Application | awk '{print $1}'`
 echo "hangingJavaProcessToStop: $hangingJavaProcessToStop"
 kill -9 $hangingJavaProcessToStop
 #ll
-cd /home/ubuntu/webapp/
-sudo su
-sudo mvn clean
